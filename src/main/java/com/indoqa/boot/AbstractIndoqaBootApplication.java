8bff209b7d52500c27fbdb23b5c3901e3ee85831
Reinhard Pötz <reinhard.poetz@indoqa.com>
2015-04-16 11:59:31 +0200

    Profile detection: improve logging

--- src/main/java/com/indoqa/boot/AbstractIndoqaBootApplication.java
+++ src/main/java/com/indoqa/boot/AbstractIndoqaBootApplication.java
@@ -175,7 +175,7 @@ public abstract class AbstractIndoqaBootApplication {
     private void initializeProfile() {
         if (this.hasNoActiveProfile()) {
             String detectedProfile = this.isDevEnvironment() ? "dev" : "prod";
-            this.logger.info("Detected Spring profile: {}", detectedProfile);
+            this.logger.info("Explictely set Spring profile: {}", detectedProfile);
             this.context.getEnvironment().setActiveProfiles(detectedProfile);
         }
         this.logger.info("Active Spring profile(s): {}", String.join(" & ", this.context.getEnvironment().getActiveProfiles()));
