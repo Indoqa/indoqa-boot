5981c79c4f7edce9abfdc17d7e1a1b83b086ef9e
Reinhard Pötz <reinhard.poetz@indoqa.com>
2016-04-03 13:57:09 +0200

    Javascript/Webpack SinglePageApplications

---
+++ src/main/java/com/indoqa/boot/jsapp/JsAppUtils.java
@@ -0,0 +1,76 @@
+/*
+ * Licensed to the Indoqa Software Design und Beratung GmbH (Indoqa) under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Indoqa licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.indoqa.boot.jsapp;
+
+import com.indoqa.boot.json.JsonTransformer;
+
+import spark.Request;
+import spark.Spark;
+
+public final class JsAppUtils {
+
+    private static final String EMPTY_INITIAL_STATE = "{}";
+    private static final String RESPONSE_HEADER_CONTENT_TYPE = "Content-Type";
+    private static final String CONTENT_TYPE_HTML = "text/html";
+
+    private JsAppUtils() {
+        // hide utility class constructor
+    }
+
+    public static void jsApp(String path, Assets assets) {
+        jsApp(path, assets, null, null);
+    }
+
+    public static void jsApp(String path, Assets assets, InitialStateProvider initialState, JsonTransformer transformer) {
+        Spark.get(path, (req, res) -> {
+            String initialStateJson = createInitialStateJson(req, initialState, transformer);
+            res.header(RESPONSE_HEADER_CONTENT_TYPE, CONTENT_TYPE_HTML);
+            return createSinglePageHtml(assets.getRootElementId(), assets.getMainCss(), assets.getMainJavascript(), initialStateJson);
+        });
+    }
+
+    protected static String createInitialStateJson(Request req, InitialStateProvider initialStateProvider,
+            JsonTransformer transformer) {
+        String initialStateJson = EMPTY_INITIAL_STATE;
+        if (initialStateProvider != null && transformer != null) {
+            Object initialStateObject = initialStateProvider.initialState(req);
+            initialStateJson = transformer.render(initialStateObject);
+        }
+        return initialStateJson;
+    }
+
+    private static String createSinglePageHtml(String rootElementId, String cssFile, String javascriptFile, String initialStateJson) {
+        return new StringBuilder()
+            .append("<!DOCTYPE html><html><head>")
+            .append("<link rel=\"stylesheet\" href=\"")
+            .append(cssFile)
+            .append("\" />")
+            .append("</head>")
+            .append("<body>")
+            .append("<div id=\"")
+            .append(rootElementId)
+            .append("\"></div>")
+            .append("<script>window.__INITIAL_STATE__ = ")
+            .append(initialStateJson)
+            .append(";</script>")
+            .append("<script src=\"")
+            .append(javascriptFile)
+            .append("\"></script>")
+            .append("</body></html>")
+            .toString();
+    }
+}
---
+++ src/main/java/com/indoqa/boot/jsapp/WebpackAssetsUtils.java
@@ -0,0 +1,129 @@
+/*
+ * Licensed to the Indoqa Software Design und Beratung GmbH (Indoqa) under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Indoqa licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.indoqa.boot.jsapp;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import com.indoqa.boot.ApplicationInitializationException;
+import com.indoqa.spring.ClassPathScanner;
+
+public final class WebpackAssetsUtils {
+
+    private static final String ASSETS_PATH = "assets/*";
+    private static final ClassPathScanner SCANNER = new ClassPathScanner();
+
+    private WebpackAssetsUtils() {
+        // hide utility class constructor
+    }
+
+    public static Assets findWebpackAssets(String folder) {
+        try {
+            return new WebpackAssets(SCANNER.findFiles(createAssetsFolder(folder)));
+        } catch (IOException e) {
+            throw new ApplicationInitializationException("Error while searching for webpack assets.", e);
+        }
+    }
+
+    private static String createAssetsFolder(String folder) {
+        StringBuilder path = new StringBuilder();
+        if (!folder.startsWith("/")) {
+            path.append("/");
+        }
+        path.append(folder);
+
+        if (!folder.endsWith("/")) {
+            path.append("/");
+        }
+        path.append(ASSETS_PATH);
+
+        return path.toString();
+    }
+
+    private static String extractFilename(String path) {
+        int lastSeparator = path.lastIndexOf("/assets/");
+        int pos = Math.min(lastSeparator, path.length());
+        return path.substring(pos);
+    }
+
+    private static String urlToUri(URL url) {
+        try {
+            return url.toURI().toASCIIString();
+        } catch (URISyntaxException e) {
+            throw new ApplicationInitializationException("Error while analyzing webpack asset url.", e);
+        }
+    }
+
+    private static class WebpackAssets implements Assets {
+
+        private static final String DEFAULT_ROOT_ELEMENT_ID = "app";
+
+        private final Set<URL> assets;
+        private final String rootElementId;
+
+        public WebpackAssets(Set<URL> assets) {
+            this(assets, DEFAULT_ROOT_ELEMENT_ID);
+        }
+
+        public WebpackAssets(Set<URL> assets, String rootElementId) {
+            this.rootElementId = rootElementId;
+            this.assets = assets;
+        }
+
+        @Override
+        public String getMainCss() {
+            return this.filterAssets(uri -> uri.endsWith("css"), "CSS");
+
+        }
+
+        @Override
+        public String getMainJavascript() {
+            return this.filterAssets(uri -> uri.endsWith("js"), "CSS");
+        }
+
+        @Override
+        public String getRootElementId() {
+            return this.rootElementId;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return this.assets.isEmpty();
+        }
+
+        private String filterAssets(Predicate<String> filter, String type) {
+            List<String> filteredAssets = this.assets
+                .stream()
+                .map(WebpackAssetsUtils::urlToUri)
+                .filter(filter)
+                .map(WebpackAssetsUtils::extractFilename)
+                .collect(Collectors.toList());
+
+            if (filteredAssets.size() > 1 || filteredAssets.isEmpty()) {
+                throw new ApplicationInitializationException(
+                    "One main " + type + " asset is expected but found " + filteredAssets.size() + ".");
+            }
+
+            return filteredAssets.get(0);
+        }
+    }
+}
