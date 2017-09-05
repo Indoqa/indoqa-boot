653e5ea7b69d6632528410abeed9085a6a09c267
Alexander Eibner <alexander.eibner@indoqa.com>
2017-09-06 00:55:31 +0200

    Respond and log REST resource errors, include timestamp, uuid, error message and optional payload/error data

---
+++ src/main/java/com/indoqa/boot/resources/error/RestResourceError.java
@@ -0,0 +1,97 @@
+/*
+ * Licensed to the Indoqa Software Design und Beratung GmbH (Indoqa) under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Indoqa licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.indoqa.boot.resources.error;
+
+import java.io.Serializable;
+import java.time.Instant;
+
+public class RestResourceError implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private int status;
+
+    private Instant timestamp;
+    private String uuid;
+    private String error;
+
+    private Object payload;
+
+    private RestResourceError() {
+        super();
+    }
+
+    public static RestResourceError build(int status, Instant timestamp, String uuid, String error) {
+        return build(status, timestamp, uuid, error, null);
+    }
+
+    public static RestResourceError build(int status, Instant timestamp, String uuid, String error, Object payload) {
+        RestResourceError result = new RestResourceError();
+
+        result.setStatus(status);
+        result.setTimestamp(timestamp);
+        result.setUuid(uuid);
+        result.setError(error);
+        result.setPayload(payload);
+
+        return result;
+    }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    public String getError() {
+        return this.error;
+    }
+
+    public Object getPayload() {
+        return this.payload;
+    }
+
+    public int getStatus() {
+        return this.status;
+    }
+
+    public Instant getTimestamp() {
+        return this.timestamp;
+    }
+
+    public String getUuid() {
+        return this.uuid;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public void setPayload(Object payload) {
+        this.payload = payload;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public void setTimestamp(Instant timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+}
---
+++ src/main/java/com/indoqa/boot/resources/error/RestResoureErrorLogger.java
@@ -0,0 +1,89 @@
+/*
+ * Licensed to the Indoqa Software Design und Beratung GmbH (Indoqa) under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ * Indoqa licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.indoqa.boot.resources.error;
+
+import static java.util.Optional.ofNullable;
+import static org.apache.commons.lang3.exception.ExceptionUtils.getRootCause;
+import static org.apache.commons.lang3.exception.ExceptionUtils.getRootCauseMessage;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import spark.Request;
+
+public class RestResoureErrorLogger {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RestResoureErrorLogger.class);
+
+    private static final String UNKNOWN = "unknown";
+
+    private RestResoureErrorLogger() {
+        // hide util class constructor
+    }
+
+    public static void logException(Request req, RestResourceError error, Exception e) {
+        int status = error.getStatus();
+        String errorMessage = getErrorMessage(error);
+        String requestMessage = getRequestMessage(req);
+        switch (status / 100) {
+            case 3:
+            case 4:
+                LOGGER.info("{} {} :{} line: {}", requestMessage, errorMessage, getRootCauseMessage(e), getExceptionLocation(e));
+                LOGGER.debug("ClientErrorUUID: {}: Exception: ", error.getUuid(), e);
+                break;
+            case 2:
+                LOGGER.warn("Unexpected log of exception {} ({}), ", requestMessage, errorMessage, e);
+                break;
+            default:
+                LOGGER.error("Error executing {}. {}.", requestMessage, errorMessage, e);
+        }
+    }
+
+    private static String getErrorMessage(RestResourceError resourceError) {
+        StringBuilder result = new StringBuilder();
+        result.append("Client error: ");
+        result.append(resourceError.getStatus());
+        result.append(": '");
+        result.append(resourceError.getError());
+        result.append("' UUID: {{{ ");
+        result.append(resourceError.getUuid());
+        result.append(" }}}");
+        return result.toString();
+    }
+
+    private static String getExceptionLocation(Exception e) {
+        Throwable rootCause = getRootCause(e);
+
+        Throwable cause = ofNullable(rootCause).orElseGet(() -> e);
+        StackTraceElement[] stackTrace = cause.getStackTrace();
+        if (stackTrace == null || stackTrace.length == 0) {
+            return UNKNOWN;
+        }
+
+        StackTraceElement element = stackTrace[0];
+        return element.getFileName() + ":" + element.getLineNumber();
+    }
+
+    private static String getRequestMessage(Request req) {
+        StringBuilder result = new StringBuilder();
+        result.append(req.requestMethod());
+        result.append(" @ '");
+        result.append(req.uri());
+        result.append('\'');
+        return result.toString();
+    }
+}
